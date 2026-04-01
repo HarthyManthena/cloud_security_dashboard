6d5d2b368594b9571d4194d825d88abcfc29d14c
HarthyManthena <harthymathena@gmail.com>
2026-04-01 19:58:30 +0530

    Initial commit

--- 1.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("access_control_dataset.csv")
-print(df.columns)
--- policy_engine.py
+++
@@ -1,9 +0,0 @@
-def enforce_policy(risk, score):
-    if risk == 1 and score < 15:
-        return "CRITICAL: Block Access + Force MFA + Alert Admin"
-
-    elif risk == 1:
-        return "High Risk: Require MFA + Log Activity"
-
-    else:
-        return "Low Risk: Access Granted"
